4c6fb884c9a5640cb4f968ecbcc15f9b1700463d
Steve Han <stevehan2001@gmail.com>
2020-02-29 17:23:29 -0600

    some stuff

--- screens/BallotScreen.js
+++ screens/BallotScreen.js
@@ -1,18 +1,35 @@
 import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import * as WebBrowser from 'expo-web-browser';
 
 import { MonoText } from '../components/StyledText';
 
-const propA = {
+const props = [
+  {
+    name: 'PropA',
+    value: 'For'
+  },
+  {
+    name: 'PropB'
 
-}
+  }
+]
 
 export default function BallotScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>This is what you'll see on your ballot on election day:</Text>
+      {props.map(prop => {
+        return (
+          <View style={styles.propCard}>
+            <Text style={styles.propTitle}>{prop.name}</Text>
+            <Text>{prop.value}</Text>
+
+          </View>
+        )
+      })
+      }
 
     </View>
   );
@@ -25,6 +42,14 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 30,
     fontWeight: '800',
+  },
+  propCard: {
+    backgroundColor: 'white',
+    padding: 20,
+    margin: 10
+  },
+  propTitle: {
+    fontSize: 20,
 
   }
 });
