c1ec4f648d1f9c4685b5b88287335b41e11c2cc3
Sam Ziegelbein <sziegelbein@verizon.net>
2020-02-29 20:03:18 -0600

    updated for/against/no preference options for ballot

--- screens/BallotScreen.js
+++ screens/BallotScreen.js
@@ -13,12 +13,15 @@ export default class BallotScreen extends React.Component {
         {
           id: 0,
           name: 'PropA',
-          value: 'For'
+          description: 'The issuance of $2,900,000 of School Building Bonds for the purpose of renovating, improving, and ' + 
+          'equipping school buildings, and the levying of a tax in payment thereof.'
         },
         {
           id: 1,
-          name: 'PropB'
-
+          name: 'PropB',
+          description: 'The issuance of $93,445,000 of Road Bonds for the purpose of the construction, maintenance, and ' + 
+          'operation of an interstate highway through the southwest forested region of the county, and the levying of a ' + 
+          'tax in payment thereof.'
         }
       ]
     }
@@ -50,8 +53,9 @@ export default class BallotScreen extends React.Component {
             <View style={styles.propCard} key={prop.id}>
               <Text style={styles.propTitle}>{prop.name}</Text>
               <Text>{prop.value}</Text>
-              <Button title="Against" onPress={() => this.onUpdate(prop.id, "Against")}></Button>
               <Button title="For" onPress={() => this.onUpdate(prop.id, "For")}></Button>
+              <Button title="Against" onPress={() => this.onUpdate(prop.id, "Against")}></Button>
+              <Button title="I have no preference" onPress={() => this.onUpdate(prop.id, "I have no preference")}></Button>
             </View>
           )
         })
